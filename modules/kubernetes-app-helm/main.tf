c92774c5a915fabfc67b0539b94255a9764a1124
git4suvendu <m_andal2002@yahoo.co.in>
2022-07-02 22:34:09 +0100

    Helm Full App Deploy

--- modules/kubernetes-app-helm/main.tf
+++ modules/kubernetes-app-helm/main.tf
@@ -5,11 +5,16 @@ variable "game_app_ingress_chart_version" {
   default = "0.1.0"
 }
 
+variable "game_app_full_chart_version" {
+  default = "0.1.0"
+}
+
 variable "app_namespace" {
    description      =   "Kubernetes namespace name in which the application will be deployed "
    type = string
    default = null
 }
+
 
 
 locals {
@@ -23,40 +28,58 @@ locals {
   game_app_ingress_chart_version = var.game_app_ingress_chart_version
   game_app_ingress_release_name = "game-app-ingress-rel"
 
+  game_app_full_chart_name    = "game-app-full"
+  game_app_full_chart_version = var.game_app_full_chart_version
+  game_app_full_release_name = "game-app-full-rel"
+
 }
 
-resource "helm_release" "game_app" {
+#resource "helm_release" "game_app" {
+#
+#  name       = local.game_app_release_name
+#  repository = local.application_helm_repo
+#  chart      = local.game_app_chart_name
+#  version    = local.game_app_chart_version
+#  namespace  = var.app_namespace
+#  create_namespace = true
+#  atomic     = true
+#  timeout    = 900
+#  cleanup_on_fail = true
+#  force_update = true
+#  recreate_pods = true
+#}
+#
+#
+#resource "helm_release" "game_app_ingress" {
+#
+#  name       = local.game_app_ingress_release_name
+#  repository = local.application_helm_repo
+#  chart      = local.game_app_chart_name
+#  version    = local.game_app_chart_version
+#  namespace  = var.app_namespace
+#  create_namespace = true
+#  atomic     = true
+#  timeout    = 900
+#  cleanup_on_fail = true
+#  force_update = true
+#  recreate_pods = true
+#
+#
+#  depends_on = [helm_release.game_app]
+#}
+#
 
-  name       = local.game_app_release_name
+
+
+resource "helm_release" "game_app_full" {
+
+  name       = local.game_app_full_release_name
   repository = local.application_helm_repo
-  chart      = local.game_app_chart_name
-  version    = local.game_app_chart_version
+  chart      = local.game_app_full_chart_name
+  version    = local.game_app_full_chart_version
   namespace  = var.app_namespace
   create_namespace = true
   atomic     = true
   timeout    = 900
   cleanup_on_fail = true
-  force_update = true
-  recreate_pods = true
 }
-
-
-resource "helm_release" "game_app_ingress" {
-
-  name       = local.game_app_ingress_release_name
-  repository = local.application_helm_repo
-  chart      = local.game_app_chart_name
-  version    = local.game_app_chart_version
-  namespace  = var.app_namespace
-  create_namespace = true
-  atomic     = true
-  timeout    = 900
-  cleanup_on_fail = true
-  force_update = true
-  recreate_pods = true
-
-
-  depends_on = [helm_release.game_app]
-}
-
-
