538d9cdd78b9b97ea54c2afb1d2aea74e5fcbca7
git4suvendu <m_andal2002@yahoo.co.in>
2022-07-03 13:50:54 +0100

    Deploy Full App

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "eks_kubernetes_addons" {
   depends_on = [module.aws_alb_controller]
 }
 
-/*
+
 module "kubernetes_app_helm" {
     source                      =  "./modules/kubernetes-app-helm"
     app_namespace               =  var.fargate_app_namespace[0]
@@ -73,6 +73,8 @@ module "kubernetes_app_helm" {
   depends_on = [module.eks, module.aws_alb_controller]
 }
 
+
+/*
 #
 
 module "kubernetes_app" {
